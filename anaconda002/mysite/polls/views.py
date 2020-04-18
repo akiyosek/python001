b5726b17a6abe357dd76b727afea0fba6a2ede6a
akiyosek <akiyosek@gmail.com>
2020-04-19 00:10:24 +0900

    Django002 created and trial

---
+++ anaconda002/mysite/polls/views.py
@@ -0,0 +1,9 @@
+#from django.shortcuts import render
+
+# Create your views here.
+
+# 20200418
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, World. You're at the polls index.")
