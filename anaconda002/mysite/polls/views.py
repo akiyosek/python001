3da23930e721adc45918f47024c9bddc5a4d273d
akiyosek <akiyosek@gmail.com>
2020-06-11 23:14:28 +0900

    20200611 最新の5件の質問項目をカンマ区切り、日付順で表示するようにindexビューを修正

--- anaconda002/mysite/polls/views.py
+++ anaconda002/mysite/polls/views.py
@@ -4,9 +4,14 @@
 
 # 20200418
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-   return HttpResponse("Hello, World. You're at the polls index.")
+#   return HttpResponse("Hello, World. You're at the polls index.")
+   latest_question_list = Question.objects.order_by('pub_date')[:5]
+   output = ', '.join([q.question_text for q in latest_question_list])
+   return HttpResponse(output)
 
 def detail(request, question_id):
    return HttpResponse("You're looking at question %s." % question_id)
