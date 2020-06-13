45d4c879ae7cd1f9cdc61492ff6c7def3827ef38
akiyosek <akiyosek@gmail.com>
2020-06-13 12:04:03 +0900

    簡単なフォームを書く

--- anaconda002/mysite/polls/views.py
+++ anaconda002/mysite/polls/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -20,10 +21,11 @@ def index(request):
    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-   try:
-      question = Question.objects.get(pk=question_id)
-   except Question.DoesNotExist:
-      raise Http404("Question does not exist")
+#   try:
+#      question = Question.objects.get(pk=question_id)
+#   except Question.DoesNotExist:
+#      raise Http404("Question does not exist")
+   question = get_object_or_404(Question, pk=question_id)
    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
