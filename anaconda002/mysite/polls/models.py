a62c09bee6a113b89975005505cdc1a813e7216a
akiyosek <akiyosek@gmail.com>
2020-06-21 00:32:40 +0900

    チュートリアル7まで完了-完成

--- anaconda002/mysite/polls/models.py
+++ anaconda002/mysite/polls/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+import datetime #<- 追記
+from django.utils import timezone #<- 追記
 # Create your models here.
 # 20200419
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+ 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
