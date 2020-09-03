a6dd6cd6d7e9fc4c2b98a8afb1ca68586055f2cf
akiyosek <akiyosek@gmail.com>
2020-09-03 23:14:59 +0900

    20200903Commit データモデル変更作業中 ->Money yami

--- asprj001/utilityfee/utilityfee/views.py
+++ asprj001/utilityfee/utilityfee/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
+
+from .models import Utilityfee
+
 def index(request):
     today = str(timezone.now()).split('-')
+    utilityfee = Utilityfee.objects.all()
     context = {
         'year' : today[0],
         'month' : today[1],
